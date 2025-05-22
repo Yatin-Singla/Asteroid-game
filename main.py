e7b178f97120052d7ca4a4d3299ba9803bb0c85c
Yatin Singla <yatinsingla2@gmail.com>
2025-05-22 17:08:30 +0530

    Decouple game time and CPU time

--- main.py
+++ main.py
@@ -5,20 +5,25 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()  # Initialize the clock
+    dt = 0
     
     while True:
-        pygame.Surface.fill(screen, (0, 0, 0))  # Fill the screen with black
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        screen.fill("black")  # Fill the screen with black
         pygame.display.flip()
+
+        # limit the frame rate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
